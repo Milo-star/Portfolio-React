763f4b4f2f8bf8beb765bd28c6b90c9aaed1d8b9
Milo Bonnet <milovtt@gmail.com>
2023-05-25 14:54:57 +0200

    V1 finish

--- src/App.js
+++ src/App.js
@@ -11,15 +11,18 @@ import web3 from './images/web3.png'
 import web4 from './images/web4.png'
 import web5 from './images/web5.png'
 import web6 from './images/web6.png'
+import { useState } from 'react';
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false)
   return (
-    <main className="bg-white px-10 md:px-20 lg:px-40">
+  <div className={darkMode ? "dark" : ""}>
+    <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
       <section className="min-h-screen">
         <nav className="py-10 flex justify-end">
           <ul className='flex items-center'>
             <li>
-              <FaMoon className='cursor-pointer text-xl'/>
+              <FaMoon onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-xl dark:text-white'/>
             </li>
             <li>
               <a href='#' className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-5'>Resume</a>
@@ -32,8 +35,8 @@ const App = () => {
             <img src={bitmoji}/>
           </div>
           <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Bonnet Milo</h2>
-          <h3 className='text-2xl py-2 md:text-3xl'>Developer Full-stack.</h3>
-          <p className='text-md py-5 leading-8 text-gray-800 md:text-xl'>Young 19-year-old developer coming out of training at <span className='text-teal-500'>BeCode</span> for 7 months. I present to you my portfolio with my different projects available! I'm starting my internship at <span className='text-teal-500'>Agency Digital</span> as a Fullstack developer</p>
+          <h3 className='text-2xl py-2 md:text-3xl dark:text-white'>Developer Full-stack.</h3>
+          <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-white'>Young 19-year-old developer coming out of training at <span className='text-teal-500'>BeCode</span> for 7 months. I present to you my portfolio with my different projects available! I'm starting my internship at <span className='text-teal-500'>Agency Digital</span> as a Fullstack developer</p>
         </div>
 
         <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
@@ -51,19 +54,19 @@ const App = () => {
         </div>
 
 
-        <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-10 overflow-hidden'>
+        <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-10 overflow-hidden md:h-96 md:w-96'>
           <img src={img} alt="logo"  layout="fill" objectFit="cover"/>
        </div>
       </section>
 
       <section>
         <div className='mt-10'>
-          <h3 className='text-3xl py-1'>Service I offer</h3>
-          <p className='text-md py-Z leading-8 text-gray-80'>Since the beginning of my journey I learned to work remotely, I also learned to do design, group work</p>
+          <h3 className='text-3xl py-1 dark:text-white'>Service I offer</h3>
+          <p className='text-md py-Z leading-8 text-gray-800 dark:text-gray-200'>Since the beginning of my journey I learned to work remotely, I also learned to do design, group work</p>
         </div>
 
-        <div>
-          <div className='text-center shadow-2xl p-10 rounded-xl my-10'>
+        <div className='lg:flex gap-10'>
+          <div className='text-center shadow-2xl p-10 rounded-xl my-10 dark:bg-white'>
             <div className='flex justify-center'>
               <img src={design} alt="logo" width={100} height={100}/>
             </div>
@@ -74,7 +77,7 @@ const App = () => {
             <p className='text-gray-800 py-1'>Canva</p>
           </div>
 
-          <div className='text-center shadow-2xl p-10 rounded-xl my-10'>
+          <div className='text-center shadow-2xl p-10 rounded-xl my-10 dark:bg-white'>
             <div className='flex justify-center'>
               <img src={code} alt="logo" width={100} height={100}/>
             </div>
@@ -91,37 +94,38 @@ const App = () => {
 
       <section>
         <div>
-          <h3 className='text-3xl py-1'>Portfolio</h3>
-          <p className='text-md py-2 leading-8 text-gray-800'>Here are some projects that I realized with heart whether <span className='text-teal-500'>personal</span> or <span className='text-teal-500'>professional</span></p>
+          <h3 className='text-3xl py-1 dark:text-white'>Portfolio</h3>
+          <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>Here are some projects that I realized with heart whether <span className='text-teal-500'>personal</span> or <span className='text-teal-500'>professional</span></p>
         </div>
 
-        <div>
-          <div>
-            <img src={web1} alt="logo" />
+        <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
+          <div className='basis-1/3 flex-1'>
+            <img src={web1} alt="logo" className='rounded-lg object-cover' width={"100%"} height={"100%"} layout="responsive"/>
           </div>
 
-          <div>
-            <img src={web2} alt="logo" />
+          <div className='basis-1/3 flex-1'>
+            <img src={web2} alt="logo" className='rounded-lg object-cover' width={"100%"} height={"100%"} layout="responsive"/>
           </div>
 
-          <div>
-            <img src={web3} alt="logo" />
+          <div className='basis-1/3 flex-1'>
+            <img src={web3} alt="logo" className='rounded-lg object-cover' width={"100%"} height={"100%"} layout="responsive"/>
           </div>
 
-          <div>
-            <img src={web4} alt="logo" />
+          <div className='basis-1/3 flex-1'>
+            <img src={web4} alt="logo" className='rounded-lg object-cover' width={"100%"} height={"100%"} layout="responsive"/>
           </div>
 
-          <div>
-            <img src={web5} alt="logo" />
+          <div className='basis-1/3 flex-1'>
+            <img src={web5} alt="logo" className='rounded-lg object-cover' width={"100%"} height={"100%"} layout="responsive"/>
           </div>
 
-          <div>
-            <img src={web6} alt="logo" />
+          <div className='basis-1/3 flex-1'>
+            <img src={web6} alt="logo" className='rounded-lg object-cover' width={"100%"} height={"100%"} layout="responsive"/>
           </div>
         </div>
       </section>
     </main>
+  </div>
 
   );
 };
